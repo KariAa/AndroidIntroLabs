0cfc28828a1a3f7c42fdccaf1511825978b62c9e
KariAa <kari.aaltonen@metropolia.fi>
2018-11-27 07:21:46 +0200

    Working with branches - new branch 3 reed

--- lab06/src/main/java/com/example/lab06/MainActivity.java
+++ lab06/src/main/java/com/example/lab06/MainActivity.java
@@ -9,7 +9,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-//Working on dev branches - new one created 3 Red B
+//Working on dev branches - new one created 3 Reed
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = "DBG";  //TAG for Log
     public static final String EXTRA = "com.example.lab06";  //Key for intent
